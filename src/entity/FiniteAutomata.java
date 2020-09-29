c1f43915a844eb20c502f8f44218739ad251cc9a
Unknown <cupjoo@naver.com>
2020-09-29 09:54:57 +0900

    add check() to FiniteAutomate

--- src/entity/FiniteAutomata.java
+++ src/entity/FiniteAutomata.java
@@ -3,4 +3,9 @@ package entity;
 public abstract class FiniteAutomata {
     public abstract boolean isAccepted(String input);
     public abstract void print();
+
+    public void check(String input){
+        System.out.print(isAccepted(input) ? "Accepted" : "Rejected");
+        System.out.println(" "+input);
+    }
 }
